bb8def48fd070a1e8f3f63726afbc9aa054fd177
Maxence Puhetini <tama@MacBook-Pro-de-Maxence.local>
2020-04-21 17:40:44 +0200

    milieu-13

--- 13/prenom.js
+++ 13/prenom.js
@@ -1,11 +1,22 @@
-// si le cookie n'existe pas, on le crée 
+// on récupère la div qui affichera le nom de l'utilisateur
+var prenomUtilisateur = document.getElementById("container")
+
+// on récupère le bouton qui permettra de réinitialiser le prénom
+document.getElementById("reinit").addEventListener("click", function(){eraseCookie("prenom")});
+
+// si le cookie n'existe pas, on appelle la fonction getPrenom
 if(readCookie("prenom") == null)
 {
-    createCookie("prenom", getPrenom(), 3);
+    getPrenom();
+}
+else // sinon on affiche son contenu dans la div récupérée plus tôt
+{
+    prenomUtilisateur.insertAdjacentElement = " " + readCookie("prenom");
 }
 
-// récupère le prénom de l'utilisateur
+// la fonction getPrenom récupère le prénom de l'utilisateur
 function getPrenom()
 {
-    prompt("Entrez votre prénom: ");
+    prenomUtilisateur = prompt("Entrez votre prénom: ");
+    createCookie("prenom", prenomUtilisateur, 3);
 }
