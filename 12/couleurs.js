17b32216eeca101ae12fe7973fcbed955dd350e2
Maxence Puhetini <tama@MacBook-Pro-de-Maxence.local>
2020-04-21 16:08:24 +0200

    màj 11

--- 12/couleurs.js
+++ 12/couleurs.js
@@ -1,11 +1,10 @@
 // on récupère le body
-var body  =  document.getElementsByTagName("body")[0];
+var bodyElement  =  document.getElementsByTagName("body")[0];
 
 // on récupère tous les boutons en appelant directement la fonction si l'évènement se produit
 document.getElementById("btnRouge").addEventListener("click", function(){changerCouleur(255,0,0)});
 document.getElementById("btnVert").addEventListener("click", function(){changerCouleur(0,255,0)});
 document.getElementById("btnVert").addEventListener("click", function(){changerCouleur(0,0,255)});
-
 
 // on récupère tous les inputs
 var inputs = document.querySelectorAll("input");
@@ -13,11 +12,18 @@ var inputs = document.querySelectorAll("input");
 // on ajoute un écouteur à chaque input -> qui appelera la fonction
 for(let i=0; i<3; i++)
 {
-    inputs[i].addEventListener("change", function(){changerCouleur(rouge, vert, bleu)});
+    inputs[i].addEventListener("change", function(){changerCouleur(inputs[0].value, inputs[1].value, inputs[3].value)});
 }
 
+
+// fonction qui changera la couleur du body
 function changerCouleur(rouge, vert, bleu)
 {
-    
-    body.style.backgroundColor = "rgb("+rouge+", "+vert+", "+bleu+")";
+    bodyElement.style.backgroundColor = "rgb("+rouge+", "+vert+", "+bleu+")";
+}
+
+// on crée un cookie si il n'existe pas déjà
+if(readCookie("couleurActuelle") == null)
+{
+    createCookie("couleurActuelle", bodyElement.style.backgroundColor, 3);
 }
