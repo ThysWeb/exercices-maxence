002a9c2c0031523a01f6b8c154a6ff8f10ed9691
Maxence Puhetini <tama@MacBook-Pro-de-Maxence.local>
2020-04-21 15:56:52 +0200

    12-màj

--- 12/couleurs.js
+++ 12/couleurs.js
@@ -1,10 +1,10 @@
 // on récupère le body
 var body  =  document.getElementsByTagName("body")[0];
 
-// on récupère tous les boutons
-var btnRouge =  document.getElementById("btnRouge");
-var btnVert  =  document.getElementById("btnVert");
-var btnBleu  =  document.getElementById("btnBleu");
+// on récupère tous les boutons en appelant directement la fonction si l'évènement se produit
+document.getElementById("btnRouge").addEventListener("click", function(){changerCouleur(255,0,0)});
+document.getElementById("btnVert").addEventListener("click", function(){changerCouleur(0,255,0)});
+document.getElementById("btnVert").addEventListener("click", function(){changerCouleur(0,0,255)});
 
 
 // on récupère tous les inputs
@@ -18,5 +18,6 @@ for(let i=0; i<3; i++)
 
 function changerCouleur(rouge, vert, bleu)
 {
+    
     body.style.backgroundColor = "rgb("+rouge+", "+vert+", "+bleu+")";
 }
