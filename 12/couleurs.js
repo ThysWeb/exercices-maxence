de837d44720d2e04c1bc4b581b47502206e22cec
Maxence Puhetini <tama@MacBook-Pro-de-Maxence.local>
2020-04-21 15:49:21 +0200

    11

--- 12/couleurs.js
+++ 12/couleurs.js
@@ -1,4 +1,22 @@
-var btn_rouge = document.getElementById("btn-rouge");
-var btn_bleu = document.getElementById("btn-bleu");
-var btn_vert = document.getElementById("btn-vert");
+// on récupère le body
+var body  =  document.getElementsByTagName("body")[0];
 
+// on récupère tous les boutons
+var btnRouge =  document.getElementById("btnRouge");
+var btnVert  =  document.getElementById("btnVert");
+var btnBleu  =  document.getElementById("btnBleu");
+
+
+// on récupère tous les inputs
+var inputs = document.querySelectorAll("input");
+
+// on ajoute un écouteur à chaque input -> qui appelera la fonction
+for(let i=0; i<3; i++)
+{
+    inputs[i].addEventListener("change", function(){changerCouleur(rouge, vert, bleu)});
+}
+
+function changerCouleur(rouge, vert, bleu)
+{
+    body.style.backgroundColor = "rgb("+rouge+", "+vert+", "+bleu+")";
+}
