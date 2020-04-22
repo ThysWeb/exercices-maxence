ca4e85e12ac8e9e5a3db930a348d26b1b1d25727
Maxence Puhetini <tama@MacBook-Pro-de-Maxence.local>
2020-04-22 09:28:24 +0200

    14-select

--- 14/exo-select.js
+++ 14/exo-select.js
@@ -1,3 +1,11 @@
 // on récupère le select
 var menuDeroulant = document.getElementById("menu-deroulant");
-var 
+
+// on récupère les options
+var opts = document.querySelectorAll("option");
+
+for(let i=0; i<2; i++)
+{
+    // on ajoute un addEventListenner
+    opts[i].addEventListener("click",function(){toggle(1000, function())})
+}
